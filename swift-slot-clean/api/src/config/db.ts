1d03e27946dd35d32763f85676cd328d523a6647
chidera <156678425+princess-21@users.noreply.github.com>
2025-10-06 22:07:37 +0100

    Update db.ts

--- swift-slot-clean/api/src/config/db.ts
+++ swift-slot-clean/api/src/config/db.ts
@@ -1,7 +1,7 @@
 
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize("swiftslot", "root", "password", {
+const sequelize = new Sequelize("swiftslot", "root", "insert_your_password", {
   host: "localhost",
   dialect: "mysql",
   logging: false, 
